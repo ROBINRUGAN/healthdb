d86d7e6c775b2d398710a3497e977c711a3ff836
吴荣榜 <1220045015@qq.com>
2024-06-27 03:23:28 +0800

    add: 我的订单还原完毕

---
+++ src/components/OrderItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hospital-badge">
+    <img src="@/assets/hosp/demo.jpg" alt="" />
+    <div class="content">
+      <div class="badge-header">
+        <h3>福建医科大学附属协和医院</h3>
+      </div>
+      <div class="badge-content">
+        <p>就诊人: {{ name }}</p>
+        <p>就诊时间: {{ time }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const name = ref('林黄骁')
+const time = ref('2024-06-30')
+</script>
+
+<style scoped>
+.hospital-badge {
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  margin: 10px 0px;
+  padding: 20px 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.badge-header h3 {
+  margin: 0;
+  color: #000000;
+  font-size: 15px;
+  line-height: 20px;
+}
+
+.badge-content p {
+  margin: 5px 0;
+  font-size: 13px;
+  color: black;
+}
+img {
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  margin-right: 12px;
+}
+.content {
+  width: 80%;
+  line-height: 16px;
+}
+</style>
--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,7 +1,64 @@
 <script setup lang="ts">
 import { RouterView, RouterLink } from 'vue-router'
+import { ref } from 'vue'
+import OrderItem from '@/components/OrderItem.vue'
+const active = ref(0)
 </script>
 
 <template>
-  <div>order</div>
+  <div class="all">
+    <van-tabs v-model:active="active" swipeable>
+      <van-tab title="全部" name="a">
+        <div class="page">
+          <OrderItem v-for="(item, index) in 10" :key="index" />
+        </div>
+      </van-tab>
+      <van-tab title="未付款" name="b">
+        <div class="page">
+          <OrderItem v-for="(item, index) in 2" :key="index" />
+        </div>
+      </van-tab>
+      <van-tab title="未开始" name="b">
+        <div class="page">
+          <OrderItem v-for="(item, index) in 5" :key="index" />
+        </div>
+      </van-tab>
+      <van-tab title="进行中" name="b">
+        <div class="page">
+          <OrderItem v-for="(item, index) in 2" :key="index" />
+        </div>
+      </van-tab>
+      <van-tab title="已完成" name="b">
+        <div class="page">
+          <OrderItem v-for="(item, index) in 1" :key="index" />
+        </div>
+      </van-tab>
+    </van-tabs>
+  </div>
 </template>
+
+<style scoped>
+.all {
+  min-height: 100vh;
+  padding-left: 5%;
+  padding-right: 5%;
+  padding-top: 15px;
+  margin: auto;
+  padding-bottom: 60px;
+  background-color: rgb(245, 240, 250);
+}
+.page {
+  min-height: 100vh;
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 10px;
+  margin-top: 10px;
+  padding-top: 1px;
+}
+:deep(.van-tabs__nav--line) {
+  border-radius: 10px;
+}
+:deep(.van-tabs__content--animated, .van-tabs__track) {
+  border-radius: 10px;
+}
+</style>
